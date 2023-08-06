bfa191db3e2d315f6ae974dbc9b8564b5dfeecb3
bjoernbuth <bjoernbuth76@gmail.com>
2023-08-06 17:47:52 +0200

    added short names for the scripts

--- setup.py
+++ setup.py
@@ -5,10 +5,15 @@ setup(
     version="1.0.0",
     author="Bjoern",
     packages=find_packages(),
+    install_requires=["pyperclip"],
     entry_points={
         "console_scripts": [
+            # listing dir bookmarks
             "list_dir_bookmarks = src.list_dir_bookmarks.list_dir_bookmarks:list_saved_dirs",
+            "ldb = src.list_dir_bookmarks.list_dir_bookmarks:list_saved_dirs",
+            # path to clipboard
             "path_to_clippboard = src.path_to_clippboard.path_to_clippboard:copy_path_to_clipboard",
+            "ptc = src.path_to_clippboard.path_to_clippboard:copy_path_to_clipboard",
         ],
     },
 )
